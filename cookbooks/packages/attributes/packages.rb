6a6ba57a3d8b5a622647d681747385f2a776d2e8
Dave Mehrman <dmehrman@gmail.com>
2014-01-24 12:29:27 -0600

    changed package file again, server booted successfully after this change

--- cookbooks/packages/attributes/packages.rb
+++ cookbooks/packages/attributes/packages.rb
@@ -6,5 +6,4 @@
 #
 # Examples below:
 
-default[:packages] = [{:name => "media-gfx/wkhtmltopdf", :version => "0.10.0_rc2"},
-                       {:name => "dev-php/pecl-ssh2", :version => "0.11.3_rc1"}]
+default[:packages] = [{:name => "media-gfx/wkhtmltopdf", :version => "0.10.0_rc2"}]
